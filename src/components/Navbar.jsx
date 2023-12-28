87ce703f4fc509b733e9ffc51aec6e3ba1a31370
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-28 17:54:21 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,10 @@
 "use client";
 import "../styles.css";
 const Navbar = () => {
-  const NavbarLinks = {
-    home: {
-      link: function () {
-        if ((window.location.href = "/")) return;
-        else window.location.href = "/";
-      },
-    },
-    contact: {
-      link: function () {
-        if ((window.location.href = "/contact")) return;
-        else window.location.href = "/contacts";
-      },
-    },
-  };
-
   return (
     <section>
       <div className="button-container">
-        <button className="button" onClick={NavbarLinks.home.link}>
+        <a className="button" href="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="1em"
@@ -32,7 +17,7 @@ const Navbar = () => {
           >
             <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
           </svg>
-        </button>
+        </a>
         <button className="button">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -52,7 +37,7 @@ const Navbar = () => {
             ></path>
           </svg>
         </button>
-        <button className="button" onClick={NavbarLinks.contact.link}>
+        <a className="button" href="/contact">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="1em"
@@ -65,7 +50,7 @@ const Navbar = () => {
           >
             <path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z"></path>
           </svg>
-        </button>
+        </a>
 
         <a
           href="https://drive.google.com/u/0/uc?id=19iYTIl0gvQQL0Ujl2zvTypZ7XxUuqRgI&export=download"
